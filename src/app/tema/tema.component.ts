47b5e61f9016e2b44d62a962d82ad353d7644b62
davansep <priscila.davanse@gmail.com>
2021-11-11 23:12:55 -0300

    [Task BlogDavanse: MODIFY componente tema] 11.11.2021

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -7,37 +7,25 @@ import { TemaService } from '../service/tema.service';
 @Component({
   selector: 'app-tema',
   templateUrl: './tema.component.html',
-  styleUrls: ['./tema.component.css']
+  styleUrls: ['./tema.component.css'],
 })
 export class TemaComponent implements OnInit {
+  tema: Tema = new Tema();
+  listaTemas: Tema[];
 
-  tema: Tema = new Tema()
-  listaTemas: Tema[]
-
-  constructor(
-    private router: Router,
-    private temaService: TemaService
-  ) { }
+  constructor(private router: Router, private temaService: TemaService) {}
 
   ngOnInit() {
-    if(environment.token == ''){
-      this.router.navigate(['/entrar'])
+    if (environment.token == '') {
+      this.router.navigate(['/entrar']);
     }
-    this.findAllTemas()
   }
 
-  cadastrar(){
+  cadastrar() {
     this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
-      this.tema = resp
-      alert('Tema cadastrado com sucesso!')
-      this.findAllTemas()
-      this.tema = new Tema
-    })
-  }
-
-  findAllTemas(){
-    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
-      this.listaTemas = resp
-    })
+      this.tema = resp;
+      alert('Tema cadastrado com sucesso!');
+      this.tema = new Tema();
+    });
   }
 }
