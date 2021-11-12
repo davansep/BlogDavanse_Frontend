7432d6baaedcb260b86fc0abff53e470d66df1c8
davansep <priscila.davanse@gmail.com>
2021-11-11 22:19:40 -0300

    [Task BlogDavanse: MODIFY componente tema] 11.11.2021

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -23,13 +23,21 @@ export class TemaComponent implements OnInit {
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
+    this.findAllTemas()
   }
 
   cadastrar(){
-    this.temaService.postTema(this.tema).subscribe((resp:Tema) =>{
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
       this.tema = resp
       alert('Tema cadastrado com sucesso!')
-      this.tema = new Tema()
+      this.findAllTemas()
+      this.tema = new Tema
+    })
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
     })
   }
 }
